b411ab94748254b7701a086e419b78c263721f04
Gilad Weinberger <giladwe21@gmail.com>
2025-05-06 21:24:46 +0300

    Add Vercel Speed Insights: integrate @vercel/speed-insights for performance monitoring in Providers component and update package dependencies.

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,12 +1,14 @@
 "use client";
 import { AuthProvider } from "@/context/AuthContext";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Providers({ children }) {
   return (
     <>
       <AuthProvider>{children}</AuthProvider>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
