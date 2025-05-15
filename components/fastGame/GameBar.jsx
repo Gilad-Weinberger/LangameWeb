06a85397d5648b8dd69a46c0c4b3fa3b5f84f96e
Gilad Weinberger <giladwe21@gmail.com>
2025-05-15 18:49:23 +0300

    Enhance GameBar component: add conditional rendering for wordList to prevent errors when empty, and introduce createWordListForRoom function to initialize wordList in Firestore if absent, improving game setup reliability.

--- components/fastGame/GameBar.jsx
+++ components/fastGame/GameBar.jsx
@@ -11,9 +11,8 @@ const GameBar = ({ room }) => {
     <div className="flex flex-1 flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-2xl font-bold mb-4">חדר משחק:</h1>
       <div className="flex flex-col items-center justify-center">
-        {wordList.map((word, index) => (
-          <div key={index}>{word}</div>
-        ))}
+        {wordList &&
+          wordList.map((word, index) => <div key={index}>{word}</div>)}
       </div>
     </div>
   );
